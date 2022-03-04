199a421eeab848e9965fe969c3abf4bd43725434
daniel bjorke <dbjo@equinor.com>
2022-03-04 11:08:18 +0100

    <feat>Added styling to loginScreen

--- src/components/authentication/LoginButton.tsx
+++ src/components/authentication/LoginButton.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import { View } from 'react-native';
-import Button from '../common/atoms/Button';
+import { StyleSheet, View } from 'react-native';
 import { msalIsConnected, msalLogin } from '../../services/auth';
 
+import Button from '../common/atoms/Button';
+import React from 'react';
+import colors from 'src/stylesheets/colors';
 
 export default function LoginButton(props: {
   scope: string;
   navigation: any;
   mainRoute: string;
 }) {
-  
   return (
     <View>
       <Button
         disabled={!msalIsConnected()}
         title="Login"
         onPress={async () => {
-          msalLogin(props.scope).then(() => props.navigation.navigate(props.mainRoute))
+          msalLogin(props.scope).then(() =>
+            props.navigation.navigate(props.mainRoute)
+          );
         }}
+        viewStyle={styles.buttonStyle}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    backgroundColor: colors.RED_LOGO,
+  },
+  buttonStyleDisabled: {
+    backgroundColor: colors.GRAY_3,
+  },
+  buttonTextStyleDisabled: {},
+});
