c37f47841db37aa71b5e9bf0a06329104c314547
Tormod Aase <22524448+tormodAase@users.noreply.github.com>
2022-03-08 15:48:25 +0100

    Login screen for expense and pay

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -6,6 +6,7 @@ import { authenticateSilently } from '../services/auth';
 import colors from '../stylesheets/colors';
 import equinorLogo from '../resources/images/equinor_logo.png';
 import { msalIsConnected } from '../services/auth';
+import { Typography } from '../components/common';
 
 //import * as WebBrowser from 'expo-web-browser';
 
@@ -14,6 +15,8 @@ export default function LoginScreen(props: {
   navigation: any;
   bundleIdentifier: string;
   mainRoute: string;
+  eds?: boolean;
+  title?: string;
   logo: any;
 }) {
   useEffect(() => {
@@ -22,7 +25,24 @@ export default function LoginScreen(props: {
         .catch((e) => console.warn(e))
         .then((res) => res && props.navigation.navigate(props.mainRoute));
   }, []);
-
+  if (props.eds && props.title) {
+    return (
+      <View style={stylesEDS.container}>
+        <Typography variant="h1" bold color={'#3D3D3D'}>{props.title}</Typography>
+        <Image
+          source={props.logo}
+          resizeMode="contain"
+          style={{ height: 400, width: 400 }}
+        />
+        <LoginButton
+          scope={props.scope}
+          navigation={props.navigation}
+          mainRoute={props.mainRoute}
+          eds
+        />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <View style={styles.splashTop}>
@@ -70,5 +90,16 @@ const styles = StyleSheet.create({
     flex: 3,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+});
+
+const stylesEDS = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingVertical: 72,
   },
 });
