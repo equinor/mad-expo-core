9ae1f70cd4bcace896650231d4ff917e5483e282
daniel bjorke <dbjo@equinor.com>
2022-03-16 13:12:35 +0100

    feat: Updated settings screen design

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,10 +1,64 @@
-import { ScrollView, View } from 'react-native';
-import { Button, Typography } from '../components/common';
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { MaterialIcons } from '@expo/vector-icons';
-import Colors from '../stylesheets/colors';
 import { getAccount, logout } from '../services/auth';
+
+import Colors from '../stylesheets/colors';
 import type { MSALAccount } from 'react-native-msal';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Typography } from '../components/common';
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  sectionMarginTop: {
+    marginTop: 10,
+  },
+  sectionSeparator: {
+    borderColor: Colors.BORDER,
+    borderBottomWidth: 1,
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    borderColor: Colors.BORDER,
+    borderBottomWidth: 1,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  sectionHeaderContainer: {
+    height: 30,
+  },
+  itemText: {
+    lineHeight: 30,
+    fontSize: 16,
+    color: Colors.EQUINOR_PRIMARY,
+    alignSelf: 'flex-start',
+  },
+  textStyle: {
+    color: Colors.GRAY_1,
+    fontSize: 16,
+    alignSelf: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  viewStyle: {
+    paddingHorizontal: 0,
+    paddingVertical: 0,
+    justifyContent: 'flex-start',
+  },
+  clearTextStyle: {
+    color: Colors.RED,
+  },
+});
 
 const SettingsScreen = (props: {
   config: Array<{ icon: string; title: string; route: string }>;
@@ -15,13 +69,13 @@ const SettingsScreen = (props: {
     getAccount().then((acc) => {
       console.log(acc.claims);
       setAccount(acc);
-    })
-  }, [])
+    });
+  }, []);
 
   const config = props.config;
   return (
-    <ScrollView>
-      <View style={{ padding: 24 }}>
+    <ScrollView style={[styles.page, styles.sectionMarginTop]}>
+      <View style={{ flex: 1 }}>
         {config.map((item, index) => (
           <Setting
             key={index}
@@ -31,15 +85,31 @@ const SettingsScreen = (props: {
             navigation={props.navigation}
           />
         ))}
-        <View style={{ paddingTop: 16 }}>
-          <Typography bold>Signed in as:</Typography>
-          {account && <Typography>
-            {account.username}
-          </Typography>}
+        {account && (
+          <View
+            style={[
+              styles.sectionSeparator,
+              styles.itemContainer,
+              styles.sectionMarginTop,
+            ]}
+          >
+            <Typography>Logged in as: {account.username}</Typography>
+          </View>
+        )}
+        <View
+          style={[
+            styles.sectionSeparator,
+            styles.sectionMarginTop,
+            { backgroundColor: 'white' },
+          ]}
+        >
           <Button
-            title="Sign out"
+            title="Log out"
+            color={Colors.RED}
             onPress={() => {
-              logout().catch(e => console.warn(e)).then(() => props.navigation.navigate("Login"))
+              logout()
+                .catch((e) => console.warn(e))
+                .then(() => props.navigation.navigate('Login'));
             }}
           />
         </View>
@@ -55,18 +125,24 @@ const Setting = (props: {
   navigation: any;
 }) => {
   return (
-    <MaterialIcons.Button
-      name={props.icon}
-      onPress={() => props.navigation.navigate(props.route)}
-      backgroundColor="transparent"
-      color="#007079"
-      underlayColor={Colors.GREEN_LIGHT}
-      style={{ padding: 12 }}
-    >
-      <Typography medium color="#007079">
-        {props.title}
-      </Typography>
-    </MaterialIcons.Button>
+    <TouchableOpacity onPress={() => props.navigation.navigate(props.route)}>
+      <View
+        style={[
+          styles.sectionSeparator,
+          styles.itemContainer,
+          { flexDirection: 'row-reverse' },
+        ]}
+      >
+        <MaterialIcons
+          name={props.icon}
+          backgroundColor="transparent"
+          color="#007079"
+        ></MaterialIcons>
+        <Typography medium color="#007079">
+          {props.title}
+        </Typography>
+      </View>
+    </TouchableOpacity>
   );
 };
 export default SettingsScreen;
