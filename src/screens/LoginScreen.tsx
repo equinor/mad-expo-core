dac7310839d6037d2205bdd92fe674f234045a89
Dan André Svendsen <47424077+dansvend@users.noreply.github.com>
2022-05-20 15:23:40 +0200

    fix: Improve auth flexibility (#58)
    Make authentication more flexible and usable by the public. Allow multiple scopes and move redirectUri logic out.
    
    Closes #57

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -5,23 +5,23 @@ import LoginButton from '../components/authentication/LoginButton';
 import { authenticateSilently } from '../services/auth';
 import colors from '../stylesheets/colors';
 import equinorLogo from '../resources/images/equinor_logo.png';
-import { msalIsConnected } from '../services/auth';
+import { isMsalConnected } from '../services/auth';
 import { Typography } from '../components/common';
 
 //import * as WebBrowser from 'expo-web-browser';
 
 export default function LoginScreen(props: {
-  scope: string;
-  navigation: any;
   bundleIdentifier: string;
+  logo: any;
   mainRoute: string;
+  navigation: any;
+  scopes: string[];
   eds?: boolean;
   title?: string;
-  logo: any;
 }) {
   useEffect(() => {
-    msalIsConnected() &&
-      authenticateSilently(props.scope)
+    isMsalConnected() &&
+      authenticateSilently(props.scopes)
         .catch((e) => console.warn(e))
         .then((res) => res && props.navigation.navigate(props.mainRoute));
   }, []);
@@ -35,7 +35,7 @@ export default function LoginScreen(props: {
           style={{ height: 400, width: 400 }}
         />
         <LoginButton
-          scope={props.scope}
+          scopes={props.scopes}
           navigation={props.navigation}
           mainRoute={props.mainRoute}
           eds
@@ -54,7 +54,7 @@ export default function LoginScreen(props: {
         </View>
         <View style={styles.splashAction}>
           <LoginButton
-            scope={props.scope}
+            scopes={props.scopes}
             navigation={props.navigation}
             mainRoute={props.mainRoute}
           />
