e2fb64c299c9d6a8f2603cf48f60d2d39ac5a6c1
havn <havn@equinor.com>
2022-08-19 14:37:29 +0200

    chore: cherrypick feedbackscreen from settingscreenredesign

--- src/screens/FeedbackScreen.tsx
+++ src/screens/FeedbackScreen.tsx
@@ -1,35 +1,129 @@
-import { ScrollView, TextInput, View } from 'react-native';
+import * as Device from 'expo-device';
+
 import { Button, Typography } from '../components/common';
 import React, { useEffect } from 'react';
-import * as Device from 'expo-device';
+import {
+  Platform,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
+import { authenticateSilently, getAccount } from '../services/auth';
+
+import { Banner } from 'mad-expo-core';
+import Colors from '../stylesheets/colors';
+import type { MSALAccount } from 'react-native-msal';
 //import * as Localization from 'expo-localization';
 import { useState } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const FeedbackScreen = (props: { loginStorageKey: string; navigation }) => {
+const styles = StyleSheet.create({
+  textStyle: {
+    color: Colors.WHITE,
+    fontSize: 16,
+  },
+  viewErrorStyle: {
+    padding: 12,
+    paddingTop: 24,
+    backgroundColor: Colors.RED,
+  },
+  viewSuccessStyle: {
+    padding: 12,
+    paddingTop: 24,
+    backgroundColor: Colors.EQUINOR_PRIMARY,
+  },
+});
+
+const FeedbackScreen = (props: {
+  locale: string;
+  timezone: string;
+  scopes: string[];
+  apiBaseUrl: string;
+  product: string;
+}) => {
   const [feedback, setFeedback] = useState('');
-  const [email, setEmail] = useState('');
+  const [bannerMessage, setBannerMessage] = useState('');
+  const [error, setError] = useState('');
+  const [isBusy, setIsBusy] = useState(false);
+  const [account, setAccount] = useState<MSALAccount>(null);
+  useEffect(() => {
+    getAccount().then((acc) => {
+      setAccount(acc);
+    });
+  }, []);
   const userData: { [key: string]: string } = {
-    'User': email,
-    'Device brand': `${Device.brand}`,
-    'Device id': `${Device.modelName}`,
+    'User': `${account?.username.substring(0, account?.username.indexOf('@'))}`,
+    'Device brand': `${Platform.OS === 'web' ? 'web' : Device.brand}`,
+    'Device': `${Platform.OS === 'web' ? 'web' : Device.modelName} `,
     'Operating system': `${Device.osName} ${Device.osVersion}`,
-    'Timezone': 'TIMEZONE',//Localization.timezone,
-    'Locale': 'TIMEZONE',//Localization.locale,
+    'Timezone': `${props?.timezone}`,
+    'Locale': `${props?.locale}`,
     'Feedback': feedback,
   };
 
-  useEffect(() => {
-    AsyncStorage.getItem(props.loginStorageKey).then((str) =>
-      setEmail(JSON.parse(str).user.email)
-    );
-  }, [props.loginStorageKey]);
-
-  function sendFeedback() {
-    //TODO send userData object
+  interface feedbackData {
+    product: string;
+    user: string;
+    msg: string;
+    systemMsg: string;
   }
+
+  const sendFeedback = (data: feedbackData) => {
+    setIsBusy(true);
+    authenticateSilently(props.scopes).then((r) =>
+      fetch(`${props.apiBaseUrl}/feedback`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${r.accessToken}`,
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            setIsBusy(false);
+            setFeedback('');
+            setBannerMessage('Thank you! Feedback sent.');
+            setTimeout(() => setBannerMessage(''), 2000);
+          }
+        })
+        .catch((error) => {
+          setIsBusy(false);
+          setError(error);
+          console.error(error);
+          setBannerMessage('Error sending your feedback.');
+        })
+    );
+  };
+
+  const getSystemMessage = (): string => {
+    let systemMsg = '\n\n';
+    const feedbackItems = [
+      'Device brand',
+      'Device',
+      'Operating system',
+      'Timezone',
+      'Locale',
+    ];
+    feedbackItems.forEach(
+      (item) => (systemMsg += `*${item}:* ${userData[item]}\n`)
+    );
+
+    return `${systemMsg}`;
+  };
+
   return (
     <ScrollView>
+      {bannerMessage !== '' && (
+        <Banner
+          text={bannerMessage}
+          textStyle={styles.textStyle}
+          viewStyle={
+            error !== '' ? styles.viewErrorStyle : styles.viewSuccessStyle
+          }
+        />
+      )}
       <View style={{ padding: 24 }}>
         <Typography variant="h1" style={{ marginBottom: 8 }}>
           Have some feedback?
@@ -54,19 +148,32 @@ const FeedbackScreen = (props: { loginStorageKey: string; navigation }) => {
             paddingTop: 16,
             marginVertical: 16,
             borderRadius: 4,
+            borderWidth: 1,
+            borderColor: 'gray',
           }}
           onChangeText={(e) => setFeedback(e.toString())}
           multiline
           placeholder="Type your feedback here"
           textAlignVertical={'top'}
+          value={Platform.OS === 'web' ? feedback : undefined}
         >
-          <Typography medium>{feedback}</Typography>
+          {Platform.OS === 'web' && <Typography medium>{feedback}</Typography>}
         </TextInput>
         <Button
           title="Send"
           viewStyle={{ width: '100%' }}
-          disabled={feedback === ''}
-          onPress={sendFeedback}
+          disabled={feedback === '' || isBusy}
+          onPress={() =>
+            sendFeedback({
+              product: props.product,
+              user: `${account?.username.substring(
+                0,
+                account?.username.indexOf('@')
+              )}`,
+              msg: feedback,
+              systemMsg: getSystemMessage(),
+            })
+          }
         />
       </View>
     </ScrollView>
@@ -74,11 +181,17 @@ const FeedbackScreen = (props: { loginStorageKey: string; navigation }) => {
 };
 
 const DataField = (props: { itemKey: string; value: string }) => (
-  <View style={{ display: 'flex', flexDirection: 'row', padding: 8 }}>
-    <Typography
-      bold
-      style={{ width: '50%' }}
-    >{`- ${props.itemKey}:`}</Typography>
+  <View
+    style={{
+      display: 'flex',
+      flexDirection: 'row',
+      padding: 8,
+      borderColor: Colors.BORDER,
+      borderBottomWidth: 1,
+      marginVertical: 8,
+    }}
+  >
+    <Typography style={{ width: '50%' }}>{`${props.itemKey}:`}</Typography>
     <Typography style={{ width: '50%' }}>{props.value}</Typography>
   </View>
 );
