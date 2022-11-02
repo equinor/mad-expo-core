a57c685f1dc7108945a0aa89472c2d7b28a17717
havardnot <havn@equinor.com>
2022-11-02 15:30:01 +0100

    feat: create ReleaseNoteScreen

---
+++ src/components/common/organisms/ChangeLog.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { View, Text, Button, ScrollView, StyleSheet, Pressable } from 'react-native';
+import Spinner from '../atoms/Spinner';
+import Colors from 'src/stylesheets/colors';
+
+const featureTitle = "What's new";
+const affirmText = 'OK';
+
+const ChangeLog = (props: {
+    releaseNote: string;
+    fetching: boolean;
+    affirm: any;
+  }) => {
+
+  const renderChangeLog = (release) => {
+    const { changelogItem, subtitleHeader, changelogText, bullet, bulletList } = styles;
+
+    const changes = release.changes || [];
+    return (
+      <ScrollView style={changelogItem}>
+        <Text style={subtitleHeader}>{release.header}</Text>
+        <Text style={changelogText}>{release.subHeader}</Text>
+        <View style={{ margin: 5, marginLeft: 10 }}>
+          {changes.map((change, index) => (
+            <View key={index.toString()} style={bulletList}>
+              <Text style={bullet}>{'\u2022'}</Text>
+              <Text style={changelogText}>{change}</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    );
+  }
+
+  
+  const { container, footer, titleHeader, buttonStyle } = styles;
+
+  const { releaseNote, affirm, fetching } = props;
+
+  if (fetching) {
+    return <Spinner />;
+  }
+
+  return (
+    <View style={container}>
+      <Text style={titleHeader}>{featureTitle}</Text>
+      {renderChangeLog(releaseNote)}
+      <View style={footer}>
+        <Pressable style={buttonStyle} onPress={affirm}>
+          <Text>{affirmText}</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    justifyContent: 'center',
+    backgroundColor: Colors.GRAY_4,
+  },
+  footer: {
+    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: Colors.GRAY_2,
+    borderTopWidth: 0.5,
+  },
+  titleHeader: {
+    marginVertical: 15,
+    alignSelf: 'center',
+    fontWeight: '500',
+    fontSize: 30,
+    color: Colors.GRAY_1,
+  },
+  subtitleHeader: {
+    fontSize: 20,
+    marginVertical: 5,
+    color: Colors.GRAY_1,
+  },
+  changelogText: {
+    fontSize: 16,
+    marginVertical: 1,
+    color: Colors.GRAY_1,
+  },
+  bullet: {
+    fontSize: 16,
+    marginVertical: 1,
+    marginRight: 5,
+    color: Colors.GRAY_1,
+  },
+  buttonStyle: {
+    backgroundColor: '#0288D1',
+    margin: 20,
+  },
+  bulletList: {
+    flexDirection: 'row',
+  },
+  changelogItem: {
+    marginBottom: 15,
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+});
+
+export default ChangeLog;
---
+++ src/screens/ReleaseNoteScreen.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+
+import { useState } from 'react';
+import ChangeLog from 'src/components/common/organisms/ChangeLog';
+import * as mockData from '../resources/mock-data.json';
+
+
+const ReleaseNoteScreen = (props: {
+  name: string;
+  version: string;
+  environment: 'dev' | 'test' | 'qa' | 'prod';
+  navigation: any;
+  demoMode?: boolean;
+  languageCode?: string;
+}) => {
+  const [releaseNote, setReleaseNote] = useState(Object);
+  const [error, setError] = useState('');
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    const fetchChangelog = (() => {
+      const environment = props.environment === 'prod' ? `` : `${props.environment}/`;
+      if(props.demoMode){
+        setReleaseNote(mockData.ReleaseNotes);
+      } else{
+          fetch(
+            `https://api.statoil.com/app/mad/${environment}api/v1/ReleaseNote/${props.name}/${props.version}`
+          )
+            .then((res) => res.json().then((data) => {
+              setReleaseNote(data);
+              setFetching(false);
+            }))
+            .catch((error) => {
+              setError(error);
+              setFetching(false);
+              }
+            );
+      }
+    });
+
+    fetchChangelog();
+  }, []);
+
+  if(error){ 
+    props.navigation.navigate('Root');
+  };
+
+  return (
+    <ChangeLog releaseNote={releaseNote} fetching={fetching} affirm={() => props.navigation.navigate('Root')} />
+  );
+};
+
+export default ReleaseNoteScreen;
