e2c3645a41d5f79fdb6eb2cc63c50ef10b664027
Tormod Aase <toraas@equinor.com>
2022-11-03 17:41:15 +0100

    fix: no longer reopens service message every 5 minutes if new service message arrives

--- src/components/common/organisms/ServiceMessage.tsx
+++ src/components/common/organisms/ServiceMessage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Banner from '../molecules/Banner';
@@ -11,17 +11,72 @@ const ServiceMessage = (props: {
   const [serviceMessage, setServiceMessage] = useState<
     ServiceMessage | 'REQUEST FAILED'
   >(null);
-  const [serviceMessageShown, setServiceMessageShown] = useState<
-  boolean
->(false);
+  const [serviceMessageShown, setServiceMessageShown] = useState<boolean>(true);
   const safeAreaInsets = useSafeAreaInsets();
-  
+
+  const lastInterval = useRef<NodeJS.Timer>();
+
+  useEffect(() => {
+    function fetchServiceMessage(environment: string) {
+      fetch(
+        `https://api.statoil.com/app/mad/${environment}api/v1/ServiceMessage/${props.serviceName}`
+      )
+        .then((res) =>
+          res.json().then((data: ServiceMessage) => {
+            if (
+              !serviceMessage ||
+              (serviceMessage !== 'REQUEST FAILED' &&
+                data.alertName !== serviceMessage.alertName)
+            ) {
+              setServiceMessage(data);
+              setServiceMessageShown(true);
+            }
+          })
+        )
+        .catch(() => setServiceMessage('REQUEST FAILED'));
+    }
+
+    const environment: string =
+      props.environment.toLowerCase() === 'prod' ? `` : `${props.environment}/`;
+
+    if (!lastInterval.current) fetchServiceMessage(environment);
+    if (lastInterval.current) clearInterval(lastInterval.current);
+    lastInterval.current = setInterval(() => {
+      console.log('SERVICE MESSAGE:', serviceMessage);
+      fetchServiceMessage(environment);
+    }, 300000);
+
+    return () => {
+      clearInterval(lastInterval.current);
+    };
+  }, [serviceMessage]);
+
   const displayBanner = () => {
     if (serviceMessage === 'REQUEST FAILED')
       return (
         <>
+          <View>
+            <Banner
+              viewStyle={StyleSheet.flatten([
+                styles.bannerViewStyle,
+                {
+                  paddingTop: safeAreaInsets.top,
+                  minHeight: 80 + safeAreaInsets.top,
+                },
+              ])}
+              maxExpandedHeight={400}
+              maxNonExpandedHeight={80 + safeAreaInsets.top}
+              text={'Could not retrieve service message'}
+              onDismiss={() => setServiceMessageShown(false)}
+            />
+          </View>
+        </>
+      );
+    if (!serviceMessage || !serviceMessage.status || !serviceMessageShown)
+      return <></>;
+    return (
+      <>
         <View>
-
           <Banner
             viewStyle={StyleSheet.flatten([
               styles.bannerViewStyle,
@@ -32,56 +87,15 @@ const ServiceMessage = (props: {
             ])}
             maxExpandedHeight={400}
             maxNonExpandedHeight={80 + safeAreaInsets.top}
-            text={'Could not retrieve service message'}
-            onDismiss={() => setServiceMessageShown(true)}
+            text={serviceMessage.message}
+            onDismiss={() => setServiceMessageShown(false)}
+            url={serviceMessage.urlString}
           />
-        </View>
-        </>
-      );
-    if (!serviceMessage || !serviceMessage.status || serviceMessageShown) return <></>;
-    return (
-      <>
-      <View>
-        <Banner
-          viewStyle={StyleSheet.flatten([
-            styles.bannerViewStyle,
-            {
-              paddingTop: safeAreaInsets.top,
-              minHeight: 80 + safeAreaInsets.top,
-            },
-          ])}
-          maxExpandedHeight={400}
-          maxNonExpandedHeight={80 + safeAreaInsets.top}
-          text={serviceMessage.message}
-          onDismiss={() => setServiceMessageShown(true)}
-          url={serviceMessage.urlString}
-        />
         </View>
       </>
     );
   };
 
-  useEffect(() => {
-    function fetchServiceMessage(environment : string) {
-      fetch(
-        `https://api.statoil.com/app/mad/${environment}api/v1/ServiceMessage/${props.serviceName}`
-      )
-        .then((res) => res.json().then((data : ServiceMessage) => {
-          if(!serviceMessage || (serviceMessage !== 'REQUEST FAILED' && data.alertName !== serviceMessage.alertName)){
-            setServiceMessage(data);
-            setServiceMessageShown(false);
-          }
-        }))
-        .catch(() => setServiceMessage('REQUEST FAILED'));
-    }
-
-    const environment: string = props.environment.toLowerCase() === "prod" ? `` : `${props.environment}/`;
-    fetchServiceMessage(environment);
-    const interval = setInterval(() => fetchServiceMessage(environment), 300000);
-    return () => {
-      clearInterval(interval);
-    }
-  }, []);
   return <>{displayBanner()}</>;
 };
 
@@ -95,7 +109,6 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
     shadowColor: '#000000',
     shadowOpacity: 0.1,
-
   },
 });
 
