6ef82068547c8e364b6b573c2f7ae356f64103d5
havardnot <havn@equinor.com>
2022-11-07 12:45:13 +0100

    feat: Successful fetch releasenote from API, and dont show user release notes more than once

--- src/screens/ReleaseNoteScreen.tsx
+++ src/screens/ReleaseNoteScreen.tsx
@@ -1,4 +1,6 @@
+import { authenticateSilently } from '../services/auth';
 import React, { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { useState } from 'react';
 import ChangeLog from '../components/common/organisms/ChangeLog';
@@ -9,7 +11,9 @@ const ReleaseNoteScreen = (props: {
   name: string;
   version: string;
   environment: 'dev' | 'test' | 'qa' | 'prod';
+  scope: string;
   navigation: any;
+  versionStorageKey: string;
   demoMode?: boolean;
   languageCode?: string;
 }) => {
@@ -17,37 +21,68 @@ const ReleaseNoteScreen = (props: {
   const [error, setError] = useState('');
   const [fetching, setFetching] = useState(true);
 
+  const storeData = async (value: string | null) => {
+    try {
+      if (value) {
+        await AsyncStorage.setItem("version", value);
+      } else {
+        await AsyncStorage.removeItem("version");
+      }
+    } catch (e) {
+      // saving error
+    }
+  };
+
   useEffect(() => {
+
+    const checkVersion = async () => {
+      try {
+        const version = await AsyncStorage.getItem("version");
+        if (version === props.version) {
+          props.navigation.navigate('Root');
+        }
+      } catch (e) {
+        // error reading value
+      }
+    };
+
     const fetchChangelog = (() => {
       const environment = props.environment === 'prod' ? `` : `${props.environment}/`;
       if(props.demoMode){
         setReleaseNote(mockData.ReleaseNotes);
         setFetching(false);
       } else{
-          fetch(
-            `https://api.statoil.com/app/mad/${environment}api/v1/ReleaseNote/${props.name}/${props.version}`
-          )
+          authenticateSilently([props.scope]).then(response => {
+            fetch(`https://api.statoil.com/app/mad/${environment}api/v1/ReleaseNote/${props.name}/${props.version}`, { 
+            method: 'GET', 
+            headers: new Headers({
+                'Authorization': response ? `Bearer ${response.accessToken}` : "", 
+            }),
+          })
             .then((res) => res.json().then((data) => {
               setReleaseNote(data);
               setFetching(false);
             }))
-            .catch((error) => {
-              setError(error);
-              setFetching(false);
-              }
-            );
+          }).catch((error) => {
+            setError(error);
+            setFetching(false);
+            }
+          );
       }
     });
-
+    
+    checkVersion();
     fetchChangelog();
   }, []);
-
   if(error){ 
     props.navigation.navigate('Root');
   };
 
   return (
-    <ChangeLog releaseNote={releaseNote} fetching={fetching} affirm={() => props.navigation.navigate('Root')} />
+    <ChangeLog releaseNote={releaseNote} fetching={fetching} affirm={() => { 
+      storeData(props.version);
+      props.navigation.navigate('Root')
+    }} />
   );
 };
 
