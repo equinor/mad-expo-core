e8c61e6d09a17cbca53277ddb8827a0ed390069a
Tormod Aase <toraas@equinor.com>
2022-12-02 13:23:09 +0100

    fix: language not set bug

--- src/components/common/atoms/textInputWithDoneButton/DoneButtonInputAccessoryView.tsx
+++ src/components/common/atoms/textInputWithDoneButton/DoneButtonInputAccessoryView.tsx
@@ -8,18 +8,21 @@ const languages = { en: en, no: no };
 
 export const DoneButtonInputAccessoryView = (props: {
   languageCode: string;
-}) => (
-  <InputAccessoryView nativeID="keyboard-done-button">
-    <View style={styles.container}>
-      <Button
-        viewStyle={styles.buttonViewStyle}
-        textStyle={styles.buttonTextStyle}
-        onPress={() => Keyboard.dismiss()}
-        title={languages[props.languageCode]['keyboard.done']}
-      />
-    </View>
-  </InputAccessoryView>
-);
+}) => {
+  const langDict = languages[props.languageCode] ?? en;
+  return (
+    <InputAccessoryView nativeID="keyboard-done-button">
+      <View style={styles.container}>
+        <Button
+          viewStyle={styles.buttonViewStyle}
+          textStyle={styles.buttonTextStyle}
+          onPress={() => Keyboard.dismiss()}
+          title={langDict['keyboard.done']}
+        />
+      </View>
+    </InputAccessoryView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
