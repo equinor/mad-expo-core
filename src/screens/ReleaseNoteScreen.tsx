251f781a6ea7ea2186212350bd6af04f2fa58214
Håvard Notland <110901733+HavardNot@users.noreply.github.com>
2022-12-22 07:43:31 +0100

    115 releasenotescreen migrate to service portal (#116)
    Migrates release note API call to Service Portal, 
    now uses Markdown converted into HTML for the bullet points in the
    release notes

--- src/screens/ReleaseNoteScreen.tsx
+++ src/screens/ReleaseNoteScreen.tsx
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState } from 'react';
 import ChangeLog from '../components/common/organisms/ChangeLog';
 import * as mockData from '../resources/mock-data.json';
-
+import { Typography } from 'mad-expo-core';
 
 const ReleaseNoteScreen = (props: {
   name: string;
@@ -18,10 +18,9 @@ const ReleaseNoteScreen = (props: {
   demoMode?: boolean;
   languageCode?: string;
 }) => {
-  const [releaseNote, setReleaseNote] = useState(Object);
+  const [release, setRelease] = useState<Release>();
   const [error, setError] = useState('');
   const [fetching, setFetching] = useState(true);
-
   const storeData = async (value: string | null) => {
     try {
       if (value) {
@@ -35,59 +34,83 @@ const ReleaseNoteScreen = (props: {
   };
 
   useEffect(() => {
-
-    const fetchChangelog = (async () => {
-
+    const fetchChangelog = async () => {
       try {
         const version = await AsyncStorage.getItem(props.versionStorageKey);
         if (version === props.version) {
-          setFetching(false)
+          setFetching(false);
           return;
         }
       } catch (e) {
         // error reading value
       }
 
-      const environment = props.environment === 'prod' ? `` : `${props.environment}/`;
-      if(props.demoMode){
-        setReleaseNote(mockData.ReleaseNotes);
+      const environment =
+        props.environment === 'prod' ? `` : `${props.environment}/`;
+      if (props.demoMode) {
+        setRelease(mockData);
         setFetching(false);
-      } else{
-          authenticateSilently(props.scopes).then(response => {
-            fetch(`https://api.statoil.com/app/mad/${environment}api/v1/ReleaseNote/${props.name}/${props.version}`, { 
-            method: 'GET', 
-            headers: new Headers({
-                'Authorization': response ? `Bearer ${response.accessToken}` : "", 
-            }),
+      } else {
+        authenticateSilently(props.scopes)
+          .then((response) => {
+            fetch(
+              `https://api.statoil.com/app/mad/${environment}api/v1.1/ReleaseNote/${props.name}/${props.version}`,
+              {
+                method: 'GET',
+                headers: new Headers({
+                  Authorization: response
+                    ? `Bearer ${response.accessToken}`
+                    : '',
+                }),
+              }
+            )
+              .then((res) =>
+                res.json().then((data) => {
+                  setRelease(() => ({
+                    ...data,
+                  }));
+                  setFetching(false);
+                })
+              )
+              .catch((error) => {
+                setError(error);
+                setFetching(false);
+              });
           })
-            .then((res) => res.json().then((data) => {
-              setReleaseNote(data);
-              setFetching(false);
-            })).catch((error) => {
-              setError(error);
-              setFetching(false);
-            })
-          }).catch((error) => {
+          .catch((error) => {
             setError(error);
             setFetching(false);
-            }
-          );
+          });
       }
-    });
-    
+    };
+
     fetchChangelog();
   }, []);
-  if(error || (!fetching && !releaseNote.changes)){ 
+  if (error || (!fetching && !release)) {
     props.navigation.navigate(props.redirectRoute);
     return <></>;
   } else {
+    if (!release) return <Typography>Test</Typography>;
+
     return (
-      <ChangeLog releaseNote={releaseNote} fetching={fetching} affirm={() => { 
-        storeData(props.version);
-        props.navigation.navigate(props.redirectRoute)
-      }} />
+      <ChangeLog
+        release={release}
+        fetching={fetching}
+        affirm={() => {
+          storeData(props.version);
+          props.navigation.navigate(props.redirectRoute);
+        }}
+      />
     );
   }
 };
+
+export interface Release {
+  app: string;
+  version: string;
+  modified: string;
+  releaseNote: string;
+  releaseDate: string;
+}
 
 export default ReleaseNoteScreen;
